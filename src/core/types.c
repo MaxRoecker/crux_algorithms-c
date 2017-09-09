4b67a37fefc99d6252c1e6329c51ee565a6ff619
Max Naegeler Roecker <max.roecker@gmail.com>
2017-09-09 17:12:07 -0300

    Refactor trunc functions as a macro

--- src/core/types.c
+++ src/core/types.c
@@ -1,16 +1,1 @@
 #include "./types.h"
-
-flp1 CRUX__trunc_flp1 (const flp1 value) {
-  const flp1 truncated = truncf(value);
-  return truncated;
-}
-
-flp2 CRUX__trunc_flp2 (const flp2 value) {
-  const flp2 truncated = trunc(value);
-  return truncated;
-}
-
-flp4 CRUX__trunc_flp4 (const flp4 value) {
-  const flp4 truncated = truncl(value);
-  return truncated;
-}
--- src/core/types.h
+++ src/core/types.h
@@ -110,30 +110,9 @@ typedef double      flp2;
 typedef long double flp4;
 
 
-/**
- * @brief Rounds a flp1 towards zero to the nearest integer value.
- *
- * @param[in] value  A number that will be truncated.
- * @return the truncated flp1 value.
- *
- */
-flp1 CRUX__trunc_flp1 (const flp1 value);
 
-/**
- * @brief Rounds a flp2 towards zero to the nearest integer value.
- *
- * @param[in] value  A number that will be truncated.
- * @return the truncated flp2 value.
- *
- */
-flp2 CRUX__trunc_flp2 (const flp2 value);
-
-/**
- * @brief Rounds a flp2 towards zero to the nearest integer value.
- *
- * @param[in] value  A number that will be truncated.
- * @return the truncated flp2 value.
- *
- */
-flp4 CRUX__trunc_flp4 (const flp4 value);
-
+#define FLP_TRUNC(value) _Generic(value,  \
+  flp1 : ((flp1) truncf(value)),          \
+  flp2 : ((flp2) trunc(value)),           \
+  flp4 : ((flp4) truncl(value)),          \
+  default: trunc(value))
