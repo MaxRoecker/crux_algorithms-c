6c772f18863d9f046d9353087823167e43327ebd
Max Naegeler Roecker <max.roecker@gmail.com>
2017-10-16 17:53:12 -0200

    Add 'core/core' module

---
+++ src/core/core.h
@@ -0,0 +1,13 @@
+/**
+ * ---------------------------------------------------------------------------
+ * @file   core.h
+ * @brief  The package header.
+ *
+ */
+#pragma once
+
+#include "./types.h"
+#include "./errors.h"
+#include "./errcodes.h"
+#include "./results.h"
+#include "./cast.h"
