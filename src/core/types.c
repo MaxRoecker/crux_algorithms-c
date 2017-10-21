a16103b48282e2d536ea6474482eff84985ca66e
Max Naegeler Roecker <max.roecker@gmail.com>
2017-10-20 23:26:29 -0200

    Add 'Bool', 'Char' and 'Size' types

--- src/core/types.c
+++ src/core/types.c
@@ -1,1 +1,13 @@
 #include "./types.h"
+
+
+Size CRUX__popcount (uintmax_t value) {
+  Size precision = 0;
+  while (value != 0) {
+    if ((value % 2) == 1) {
+      precision += 1;
+    }
+    value >>= 1;
+  }
+  return precision;
+}
--- src/core/types.h
+++ src/core/types.h
@@ -19,97 +19,103 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <limits.h>
 
-
-
-
-typedef uint8_t IU08;
-typedef uint16_t IU16;
-typedef uint32_t IU32;
-typedef uint64_t IU64;
-
-#define IU08_INT_PRECISION UINT32_C(8)
-#define IU16_INT_PRECISION UINT32_C(16)
-#define IU32_INT_PRECISION UINT32_C(32)
-#define IU64_INT_PRECISION UINT32_C(64)
-
-#define IU08_MIN UINT8_C(0)
-#define IU16_MIN UINT16_C(0)
-#define IU32_MIN UINT32_C(0)
-#define IU64_MIN UINT64_C(0)
-
-#define IU08_MAX (UINT8_MAX)
-#define IU16_MAX (UINT16_MAX)
-#define IU32_MAX (UINT32_MAX)
-#define IU64_MAX (UINT64_MAX)
-
-#define IU08_MAGNITUDE_MAX (UINT8_MAX)
-#define IU16_MAGNITUDE_MAX (UINT16_MAX)
-#define IU32_MAGNITUDE_MAX (UINT32_MAX)
-#define IU64_MAGNITUDE_MAX (UINT64_MAX)
-
-#define IU08_MAGNITUDE_MIN UINT8_C(1)
-#define IU16_MAGNITUDE_MIN UINT16_C(1)
-#define IU32_MAGNITUDE_MIN UINT32_C(1)
-#define IU64_MAGNITUDE_MIN UINT64_C(1)
-
-#define IU08_FMT PRIu8
-#define IU16_FMT PRIu16
-#define IU32_FMT PRIu32
-#define IU64_FMT PRIu64
-
-
-
-
-typedef int8_t IS08;
-typedef int16_t IS16;
-typedef int32_t IS32;
-typedef int64_t IS64;
-
-#define IS08_INT_PRECISION UINT32_C(7)
-#define IS16_INT_PRECISION UINT32_C(15)
-#define IS32_INT_PRECISION UINT32_C(31)
-#define IS64_INT_PRECISION UINT32_C(63)
-
-#define IS08_MAX (INT8_MAX)
-#define IS16_MAX (INT16_MAX)
-#define IS32_MAX (INT32_MAX)
-#define IS64_MAX (INT64_MAX)
-
-#define IS08_MIN (INT8_MIN)
-#define IS16_MIN (INT16_MIN)
-#define IS32_MIN (INT32_MIN)
-#define IS64_MIN (INT64_MIN)
-
-#define IS08_MAGNITUDE_MAX (INT8_MAX)
-#define IS16_MAGNITUDE_MAX (INT16_MAX)
-#define IS32_MAGNITUDE_MAX (INT32_MAX)
-#define IS64_MAGNITUDE_MAX (INT64_MAX)
-
-#define IS08_MAGNITUDE_MIN INT8_C(1)
-#define IS16_MAGNITUDE_MIN INT16_C(1)
-#define IS32_MAGNITUDE_MIN INT32_C(1)
-#define IS64_MAGNITUDE_MIN INT64_C(1)
-
-#define IS08_FMT PRId8
-#define IS16_FMT PRId16
-#define IS32_FMT PRId32
-#define IS64_FMT PRId64
 
 
 
 typedef bool Bool;
 typedef char Char;
 typedef size_t Size;
+typedef uint8_t IU08;
+typedef uint16_t IU16;
+typedef uint32_t IU32;
+typedef uint64_t IU64;
+typedef int8_t IS08;
+typedef int16_t IS16;
+typedef int32_t IS32;
+typedef int64_t IS64;
 
+
+#define BOOL_MIN ((Bool) false)
+#define SIZE_MIN ((Size) 0)
+#define IU08_MIN UINT8_C(0)
+#define IU16_MIN UINT16_C(0)
+#define IU32_MIN UINT32_C(0)
+#define IU64_MIN UINT64_C(0)
+#define IS08_MIN (INT8_MIN)
+#define IS16_MIN (INT16_MIN)
+#define IS32_MIN (INT32_MIN)
+#define IS64_MIN (INT64_MIN)
+
+
+#define BOOL_MAX ((Bool) true)
+#define IU08_MAX (UINT8_MAX)
+#define IU16_MAX (UINT16_MAX)
+#define IU32_MAX (UINT32_MAX)
+#define IU64_MAX (UINT64_MAX)
+#define IS08_MAX (INT8_MAX)
+#define IS16_MAX (INT16_MAX)
+#define IS32_MAX (INT32_MAX)
+#define IS64_MAX (INT64_MAX)
+
+
+#define BOOL_MAGNITUDE_MIN ((Bool) true)
+#define CHAR_MAGNITUDE_MIN ((Char) 1)
+#define SIZE_MAGNITUDE_MIN ((Size) 1)
+#define IU08_MAGNITUDE_MIN UINT8_C(1)
+#define IU16_MAGNITUDE_MIN UINT16_C(1)
+#define IU32_MAGNITUDE_MIN UINT32_C(1)
+#define IU64_MAGNITUDE_MIN UINT64_C(1)
+#define IS08_MAGNITUDE_MIN INT8_C(1)
+#define IS16_MAGNITUDE_MIN INT16_C(1)
+#define IS32_MAGNITUDE_MIN INT32_C(1)
+#define IS64_MAGNITUDE_MIN INT64_C(1)
+
+
+#define BOOL_MAGNITUDE_MAX ((Bool) true)
+#define CHAR_MAGNITUDE_MAX (CHAR_MAX)
+#define SIZE_MAGNITUDE_MAX (SIZE_MAX)
+#define IU08_MAGNITUDE_MAX (UINT8_MAX)
+#define IU16_MAGNITUDE_MAX (UINT16_MAX)
+#define IU32_MAGNITUDE_MAX (UINT32_MAX)
+#define IU64_MAGNITUDE_MAX (UINT64_MAX)
+#define IS08_MAGNITUDE_MAX (INT8_MAX)
+#define IS16_MAGNITUDE_MAX (INT16_MAX)
+#define IS32_MAGNITUDE_MAX (INT32_MAX)
+#define IS64_MAGNITUDE_MAX (INT64_MAX)
+
+
+#define BOOL_INT_PRECISION CRUX__popcount(BOOL_MAX)
+#define CHAR_INT_PRECISION CRUX__popcount(CHAR_MAX)
+#define SIZE_INT_PRECISION CRUX__popcount(SIZE_MAX)
+#define IU08_INT_PRECISION ((Size) 8)
+#define IU16_INT_PRECISION ((Size) 16)
+#define IU32_INT_PRECISION ((Size) 32)
+#define IU64_INT_PRECISION ((Size) 64)
+#define IS08_INT_PRECISION ((Size) 7)
+#define IS16_INT_PRECISION ((Size) 15)
+#define IS32_INT_PRECISION ((Size) 31)
+#define IS64_INT_PRECISION ((Size) 63)
+
+
+#define BOOL_FMT "d"
+#define CHAR_FMT "c"
+#define SIZE_FMT "zu"
+#define IU08_FMT PRIu8
+#define IU16_FMT PRIu16
+#define IU32_FMT PRIu32
+#define IU64_FMT PRIu64
+#define IS08_FMT PRId8
+#define IS16_FMT PRId16
+#define IS32_FMT PRId32
+#define IS64_FMT PRId64
 
 
 #define CRUX__as_iu08(integer) ((IU08) UINT8_C(integer))
 #define CRUX__as_iu16(integer) ((IU16) UINT16_C(integer))
 #define CRUX__as_iu32(integer) ((IU32) UINT32_C(integer))
 #define CRUX__as_iu64(integer) ((IU64) UINT64_C(integer))
-
-
 #define CRUX__as_is08(integer) ((IS08) INT8_C(integer))
 #define CRUX__as_is16(integer) ((IS16) INT16_C(integer))
 #define CRUX__as_is32(integer) ((IS32) INT32_C(integer))
@@ -117,6 +123,8 @@ typedef size_t Size;
 
 
 #define CRUX__get_int_precision(value) _Generic(value,  \
+  Bool : BOOL_INT_PRECISION,                            \
+  Char : CHAR_INT_PRECISION,                            \
   IU08 : IU08_INT_PRECISION,                            \
   IU16 : IU16_INT_PRECISION,                            \
   IU32 : IU32_INT_PRECISION,                            \
@@ -128,6 +136,8 @@ typedef size_t Size;
 
 
 #define CRUX__get_max_of(value) _Generic(value, \
+  Bool : BOOL_MAX,                              \
+  Char : CHAR_MAX,                              \
   IU08 : IU08_MAX,                              \
   IU16 : IU16_MAX,                              \
   IU32 : IU32_MAX,                              \
@@ -135,13 +145,12 @@ typedef size_t Size;
   IS08 : IS08_MAX,                              \
   IS16 : IS16_MAX,                              \
   IS32 : IS32_MAX,                              \
-  IS64 : IS64_MAX,                              \
-  FLP1 : FLP1_MAX,                              \
-  FLP2 : FLP2_MAX,                              \
-  FLP4 : FLP4_MAX)
+  IS64 : IS64_MAX)
 
 
 #define CRUX__get_min_of(value) _Generic(value, \
+  Bool : BOOL_MIN,                              \
+  Char : CHAR_MIN,                              \
   IU08 : IU08_MIN,                              \
   IU16 : IU16_MIN,                              \
   IU32 : IU32_MIN,                              \
@@ -153,6 +162,8 @@ typedef size_t Size;
 
 
 #define CRUX__get_magnitude_max_of(value) _Generic(value, \
+  Bool : BOOL_MAGNITUDE_MAX,                              \
+  Char : CHAR_MAGNITUDE_MAX,                              \
   IU08 : IU08_MAGNITUDE_MAX,                              \
   IU16 : IU16_MAGNITUDE_MAX,                              \
   IU32 : IU32_MAGNITUDE_MAX,                              \
@@ -164,6 +175,8 @@ typedef size_t Size;
 
 
 #define CRUX__get_magnitude_min_of(value) _Generic(value, \
+  Bool : BOOL_MAGNITUDE_MIN,                              \
+  Char : CHAR_MAGNITUDE_MIN,                              \
   IU08 : IU08_MAGNITUDE_MIN,                              \
   IU16 : IU16_MAGNITUDE_MIN,                              \
   IU32 : IU32_MAGNITUDE_MIN,                              \
@@ -175,6 +188,8 @@ typedef size_t Size;
 
 
 #define CRUX__get_fmt_of(value) _Generic(value, \
+  Bool : BOOL_FMT,                              \
+  Char : CHAR_FMT,                              \
   IU08 : IU08_FMT,                              \
   IU16 : IU16_FMT,                              \
   IU32 : IU32_FMT,                              \
@@ -183,3 +198,13 @@ typedef size_t Size;
   IS16 : IS16_FMT,                              \
   IS32 : IS32_FMT,                              \
   IS64 : IS64_FMT)
+
+
+/**
+ * @brief The popcount function returns the integer precision of any integer.
+ *
+ * @param[in] value the value where bits will be count;
+ * @return the number of bits on value.
+ *
+ */
+Size CRUX__popcount (uintmax_t value);
