06a6d3a67393d62d6be24065565a5269727bb9b2
Max Naegeler Roecker <max.roecker@gmail.com>
2017-11-11 14:09:36 -0200

    Add 'arithetic' package header

---
+++ src/arithmetic/arithmetic.h
@@ -0,0 +1,13 @@
+/**
+ * ---------------------------------------------------------------------------
+ * @file   arithmetic.h
+ * @brief  The package header.
+ *
+ */
+#pragma once
+
+#include "./abs.h"
+#include "./add.h"
+#include "./sub.h"
+#include "./mul.h"
+#include "./div.h"
