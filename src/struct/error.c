8bc28b0b9a80cfa27b087f601be01c4afb15692a
Max Naegeler Roecker <max.roecker@gmail.com>
2017-12-02 14:40:50 -0200

    Add 'struct/errors' module

---
+++ src/struct/error.c
@@ -0,0 +1,6 @@
+#include "./error.h"
+
+
+const CRUX__Error CRUX__ERROR_STRUCTURE = {
+  .code = CRUX__as_iu32(0x00030000),
+  .name = "Structure Error"};
---
+++ src/struct/error.h
@@ -0,0 +1,17 @@
+/**
+ * ---------------------------------------------------------------------------
+ * @file   error.h
+ * @brief  The errors of the package.
+ * 
+ */
+#pragma once
+#include "./../arithmetic/arithmetic.h"
+
+
+
+/**
+ * CRUX__ERROR_STRUCTURE
+ * 
+ * Generic data structure error. Raised when no specific error is available.
+ */
+extern const CRUX__Error CRUX__ERROR_STRUCTURE;
